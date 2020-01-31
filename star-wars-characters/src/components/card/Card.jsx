595e84d6b8a5085f23727ab67b8df96ba6eedc5d
Ghad <zimobie@gmail.com>
2020-01-31 22:09:53 +0100

    building app layout, created App component and Card component to display character profile

---
+++ star-wars-characters/src/components/card/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = (props) => {
+  const { character } = props;
+  return (
+    <div>
+      <h2 key={8 + Math.random()}>{character.name}</h2>
+      <p>{character.gender}</p>
+      <p>{character.height}</p>
+    </div>
+  );
+}
+
+export default Card;
