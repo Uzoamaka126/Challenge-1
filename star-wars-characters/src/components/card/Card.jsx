2273e93b8c4f7178aea71e14519231d48758484a
Ghad <zimobie@gmail.com>
2020-02-01 07:14:46 +0100

    added styles to Card component using styled components

--- star-wars-characters/src/components/card/Card.jsx
+++ star-wars-characters/src/components/card/Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { CardBox } from './CardStyles';
 
 const Card = (props) => {
   const { character } = props;
   return (
-    <div>
+    <CardBox>
       <h2 key={8 + Math.random()}>{character.name}</h2>
       <p>{character.birth_year}</p>
       <p>{character.height}</p>
-    </div>
+    </CardBox>
   );
 }
 
---
+++ star-wars-characters/src/components/card/CardStyles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CardBox = styled.div`
+  width: 80%;
+  height: 200px;
+  margin: 40px auto;
+  text-align: center;
+  background-color: #ffde7c;
+  display: flex;
+  align-items: center;
+  flex-flow: column wrap;
+`;
